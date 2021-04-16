2c023f547b822b51a0c5e76bc6aae7b6e38faedd
azzahrafatimah <zahrai2f2zp29@gmail.com>
2021-04-16 10:35:00 +0700

    Quiz 7-3

---
+++ app/src/main/java/com/fatimah/quiz7_3/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.fatimah.quiz7_3
+
+import android.app.TimePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var textView:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = findViewById<TextView>(R.id.jam)
+    }
+    fun ButtonOnClicked(view: View){
+        val cal = Calendar.getInstance()
+        val timeSetListener = TimePickerDialog.OnTimeSetListener {
+            timePicker, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+            textView.text = SimpleDateFormat("HH:mm").format(cal.time)
+        }
+        TimePickerDialog(
+                this, timeSetListener,
+                cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE),
+                true
+        ).show()
+    }
+}
